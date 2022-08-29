fcdb0e376306b4a93a5dce6bdb94da81778a0a74
Zaheer Khan Niazi <zaheerniazipak@gmail.com>
2022-08-29 22:16:09 +0500

    Adding context data and use of Django variable

--- feed/views.py
+++ feed/views.py
@@ -5,3 +5,9 @@ from django.views.generic import TemplateView
 # Create your views here.
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+    # Adding custom context data
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['greeting_message'] = "Hello to the world of Programming!"
+        return context
