172d7459ef747437266f8d6527756dbefc3022bc
Zaheer Khan Niazi <zaheerniazipak@gmail.com>
2022-08-30 08:08:39 +0500

    Added Images to models using sorl-thumbnail

--- feed/models.py
+++ feed/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from sorl.thumbnail import ImageField
 
 
 # Create your models here.
 class Post(models.Model):
     text = models.CharField(max_length=140, null=False, blank=True)
+    image = models.ImageField()
 
     # how to make post title identifiable
     def __str__(self):
