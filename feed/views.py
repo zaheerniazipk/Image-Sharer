dfcff8e47ab96c2ba047369297d61d8fe660d16e
Zaheer Khan Niazi <zaheerniazipak@gmail.com>
2022-08-30 11:52:04 +0500

    Sorted Post order to Descending

--- feed/views.py
+++ feed/views.py
@@ -13,7 +13,7 @@ class HomePageView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         # Displaying dynamic Posts
-        context['posts'] = Post.objects.all()
+        context['posts'] = Post.objects.all().order_by('-id')  # LIFO
         return context
 
 
